68e2b5d5e88ff45c8ff5e7eda4936ef8e28cf349
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2024-02-08 10:13:40 +0100

    updated classes for schedule and empty GUI

---
+++ src/Objects/Schedule.java
@@ -0,0 +1,12 @@
+package Objects;
+
+import java.util.ArrayList;
+
+public class Schedule {
+    private ArrayList<Attraction> attractions = new ArrayList<>();
+    private ArrayList<Location> locations = new ArrayList<>();
+    private ArrayList<Visitor> visitors = new ArrayList<>();
+
+
+
+}
