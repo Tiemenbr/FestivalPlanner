7f6805efad860d503017750fe16fd78adfc2c8ae
tiemenbr <tiemenbr@hotmail.nl>
2024-02-08 21:54:23 +0100

    added a basic GUI
    added a basic javaFX GUI and modified the README to KermisPlanner

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,35 @@
-public class GUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
     public static void main(String[] args) {
         Planner planner = new Planner();
         planner.init();
+        launch(GUI.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Kermis planner");
+
+        TabPane tabpane = new TabPane();
+
+        Tab planner = new Tab("Planner");
+        Label plannerLabel = new Label("planner page");
+        planner.setContent(plannerLabel);
+
+        Tab schedule = new Tab("Schedule");
+        Label scheduleLabel = new Label("Visualization of the schedule page");
+        schedule.setContent(scheduleLabel);
+
+        tabpane.getTabs().addAll(planner,schedule);
+
+        Scene scene = new Scene(tabpane,1200,600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
