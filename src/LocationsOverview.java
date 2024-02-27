e6945ba5ae604462fc9894b04ed40ad6d5333b90
DaanF1 <daanstander@hotmail.nl>
2024-02-27 18:32:10 +0100

    id sorted overview locations
    locations are now sorted on id, instead of the name

--- src/LocationsOverview.java
+++ src/LocationsOverview.java
@@ -14,19 +14,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LocationsOverview{
+    private static Schedule schedule;
     private static HashMap<Integer, Location> locations;
     private static ComboBox<Location> locationComboBox = new ComboBox();
+
+    // Used for generating Location Id's
     private static int hiddenId;
+    // Location variable to temporarily save the location that is getting updated
+    private static Location theLocation;
+    // Boolean value is either true: add location, or false: update location
+    private static boolean add = true;
+    
     public static VBox getComponent(){
-        Schedule schedule = Planner.getSchedule();
+        schedule = Planner.getSchedule();
 
         VBox mainBox = new VBox();
 
         HBox listsContainerBox = new HBox();
 
         // List
-//        ListView<String> collumnIds = new ListView<>();
-//        collumnIds.getItems().add("Id");
         ListView<String> collumnNames = new ListView<>();
         collumnNames.getItems().add("Name");
         ListView<String> collumnHeights = new ListView<>();
@@ -35,30 +41,16 @@ public class LocationsOverview{
         collumnWidths.getItems().add("Width");
         ListView<String> collumnVisitors = new ListView<>();
         collumnVisitors.getItems().add("Visitors");
+        ListView<Button> collumnUpdate = new ListView<>();
+        Button buttonEmpty = new Button("Update");
+        buttonEmpty.setPrefHeight(3);
+        buttonEmpty.setDisable(true);
+        collumnUpdate.getItems().add(buttonEmpty);
 
-        listsContainerBox.getChildren().addAll(collumnNames, collumnHeights, collumnWidths, collumnVisitors);
-
-        // Fill list
-        locations = schedule.getLocations();
-        hiddenId = locations.size()+1;
-        for (int i = 0; i < locations.size(); i++) {
-            //collumnIds.getItems().add(String.valueOf(locations.get(i+1).getId()));
-            collumnHeights.getItems().add(String.valueOf(locations.get(i+1).getHeight()));
-            collumnWidths.getItems().add(String.valueOf(locations.get(i+1).getWidth()));
-            collumnNames.getItems().add(locations.get(i+1).getName());
-            if (locations.get(i+1).getVisitors().size() > 1){
-                collumnVisitors.getItems().add("True");
-            } else {
-                collumnVisitors.getItems().add("False");
-            }
-        }
+        listsContainerBox.getChildren().addAll(collumnNames, collumnHeights, collumnWidths, collumnVisitors, collumnUpdate);
 
         // Add location
         VBox locationAddBox = new VBox();
-//        HBox boxLocationId = new HBox();
-//        Label labelLocationId = new Label("ID: ");
-//        TextField textLocationId = new TextField();
-//        boxLocationId.getChildren().addAll(labelLocationId, textLocationId);
         HBox boxLocationName = new HBox();
         Label labelLocationName = new Label("Name: ");
         TextField textLocationName = new TextField();
@@ -73,95 +65,176 @@ public class LocationsOverview{
         boxLocationWidth.getChildren().addAll(labelLocationWidth, textLocationWidth);
         Button buttonLocationAdd = new Button("Add location");
         Label locationAdded = new Label();
+
+        // Fill list
+        locations = schedule.getLocations();
+        hiddenId = locations.size()+1;
+        // Walk through all locations
+        for (int i = 0; i < locations.size(); i++) {
+            collumnNames.getItems().add(locations.get(i+1).getName());
+            collumnHeights.getItems().add(String.valueOf(locations.get(i+1).getHeight()));
+            collumnWidths.getItems().add(String.valueOf(locations.get(i+1).getWidth()));
+            if (locations.get(i+1).getVisitors().size() > 1){
+                collumnVisitors.getItems().add("True");
+            } else {
+                collumnVisitors.getItems().add("False");
+            }
+            // Add update button
+            Button buttonUpdate = new Button("Update");
+            buttonUpdate.setPrefHeight(3);
+            int finalI = i+1;
+            buttonUpdate.setOnAction(event -> {
+                refreshLocations();
+                add = false;
+                theLocation = locations.get(finalI);
+                if (theLocation != null){
+                    textLocationName.setText(theLocation.getName());
+                    textLocationHeight.setText(String.valueOf(theLocation.getHeight()));
+                    textLocationWidth.setText(String.valueOf(theLocation.getWidth()));
+                } else {
+                    textLocationName.setText("");
+                    textLocationHeight.setText("");
+                    textLocationWidth.setText("");
+                }
+            });
+            collumnUpdate.getItems().add(buttonUpdate);
+            refreshLocations();
+        }
+
         buttonLocationAdd.setOnAction(event -> {
             if (!textLocationName.getText().equalsIgnoreCase("") &&
                     !textLocationHeight.getText().equalsIgnoreCase("") &&
-                    !textLocationWidth.getText().equalsIgnoreCase("")){
-                boolean add = true;
-                for (Map.Entry<Integer, Location> entry : locations.entrySet()){
-                    if (String.valueOf(entry.getValue().getName()).equalsIgnoreCase(textLocationName.getText())){
-                        add = false;
-                    }
-                }
+                    Integer.valueOf(textLocationHeight.getText()) > 0 &&
+                    !textLocationWidth.getText().equalsIgnoreCase("") &&
+                    Integer.valueOf(textLocationWidth.getText()) > 0){
                 // Adding an item
                 if (add){
-                    // Add location to lists and schedule
-                    Location location = new Location(hiddenId++, Integer.valueOf(textLocationHeight.getText()), Integer.valueOf(textLocationWidth.getText()), textLocationName.getText());
-                    locations.put(hiddenId++, location);
-                    schedule.addLocation(location);
-                    locationAdded.setText("Location added!");
-
-                    // Update list
-                    //collumnIds.getItems().add(textLocationId.getText());
-                    collumnHeights.getItems().add(textLocationHeight.getText());
-                    collumnWidths.getItems().add(textLocationWidth.getText());
-                    collumnNames.getItems().add(textLocationName.getText());
-                    collumnVisitors.getItems().add("False");
-
-                    // Update combobox
-                    locationComboBox.getItems().add(location);
-                }
-                // Updating an item
-                if (!add){
-                    int index = 0;
-                    int theIndex = 0;
-                    for (String str : collumnNames.getItems()){
-                        if (str.equalsIgnoreCase(textLocationName.getText())){
-                            theIndex = index;
-                        }
-                        index++;
-                    }
-                    int id = 0;
-                    int key = 0;
-                    Location value = null;
-                    for (Map.Entry<Integer, Location> entry : locations.entrySet()){
-                        if (String.valueOf(entry.getValue().getName()).equalsIgnoreCase(textLocationName.getText())){
-                            id = entry.getValue().getId();
-                            key = entry.getKey();
-                            value = entry.getValue();
-                        }
-                    }
-                    if (id != 0 & key != 0 && value != null){
-                        // Delete location from lists and schedule
-                        schedule.deleteLocation(id);
-                        locations.remove(key, value);
-
-                        // Update list
-                        //collumnIds.getItems().remove(theIndex);
-                        collumnHeights.getItems().remove(theIndex);
-                        collumnWidths.getItems().remove(theIndex);
-                        collumnNames.getItems().remove(theIndex);
-                        collumnVisitors.getItems().remove(theIndex);
-
-                        // Update combobox
-                        locationComboBox.getItems().remove(value);
-
+                    try {
                         // Add location to lists and schedule
-                        Location location = new Location(locations.size()+1, Integer.valueOf(textLocationHeight.getText()), Integer.valueOf(textLocationWidth.getText()), textLocationName.getText());
-                        locations.put(locations.size()+1, location);
+                        Location location = new Location(hiddenId++, Integer.valueOf(textLocationHeight.getText()), Integer.valueOf(textLocationWidth.getText()), textLocationName.getText());
+                        refreshLocations();
                         schedule.addLocation(location);
-                        locationAdded.setText("Location updated!");
+                        locationAdded.setText("Location added!");
 
                         // Update list
-                        //collumnIds.getItems().add(textLocationId.getText());
                         collumnHeights.getItems().add(textLocationHeight.getText());
                         collumnWidths.getItems().add(textLocationWidth.getText());
                         collumnNames.getItems().add(textLocationName.getText());
                         collumnVisitors.getItems().add("False");
+                        Button newButton = new Button("Update");
+                        newButton.setPrefHeight(3);
+                        newButton.setOnAction(event1 -> {
+                            refreshLocations();
+                            add = false;
+                            theLocation = locations.get(location.getId());
+                            if (theLocation != null){
+                                textLocationName.setText(theLocation.getName());
+                                textLocationHeight.setText(String.valueOf(theLocation.getHeight()));
+                                textLocationWidth.setText(String.valueOf(theLocation.getWidth()));
+                            } else{
+                                textLocationName.setText("");
+                                textLocationHeight.setText("");
+                                textLocationWidth.setText("");
+                            }
+                        });
+                        collumnUpdate.getItems().add(newButton);
 
                         // Update combobox
                         locationComboBox.getItems().add(location);
+
+                        // Update textboxes
+                        textLocationName.setText("");
+                        textLocationHeight.setText("");
+                        textLocationWidth.setText("");
+
+                        refreshLocations();
+                        add = true;
+                    } catch (Exception e){
+                        locationAdded.setText("Location could not be added!");
+                        e.printStackTrace();
+                    }
+                }
+                // Updating an item
+                else if (!add){
+                    try {
+                        // Get id
+                        int index = 0;
+                        int theIndex = 0;
+                        for (Map.Entry<Integer, Location> entry : locations.entrySet()){
+                            index++;
+                            if (entry.getValue() == theLocation){
+                                theIndex = index;
+                            }
+                        }
+                        // Delete location from lists and schedule
+                        schedule.deleteLocation(theLocation.getId());
+                        locations.remove(theLocation.getId(), theLocation);
+
+                        // Update lists
+                        collumnHeights.getItems().remove(theIndex);
+                        collumnWidths.getItems().remove(theIndex);
+                        collumnNames.getItems().remove(theIndex);
+                        collumnVisitors.getItems().remove(theIndex);
+                        collumnUpdate.getItems().remove(theIndex);
+
+                        refreshLocations();
+
+                        // Update combobox
+                        locationComboBox.getItems().remove(theLocation);
+
+                        // Add location to lists and schedule
+                        Location location = new Location(hiddenId++, Integer.valueOf(textLocationHeight.getText()), Integer.valueOf(textLocationWidth.getText()), textLocationName.getText());
+                        // Recheck hiddenId value
+                        refreshLocations();
+                        schedule.addLocation(location);
+                        locationAdded.setText("Location updated!");
+
+                        // Update lists
+                        collumnHeights.getItems().add(textLocationHeight.getText());
+                        collumnWidths.getItems().add(textLocationWidth.getText());
+                        collumnNames.getItems().add(textLocationName.getText());
+                        collumnVisitors.getItems().add("False");
+                        Button newButton = new Button("Update");
+                        newButton.setPrefHeight(3);
+                        newButton.setOnAction(event1 -> {
+                            refreshLocations();
+                            add = false;
+                            theLocation = locations.get(location.getId());
+                            if (theLocation != null){
+                                textLocationName.setText(theLocation.getName());
+                                textLocationHeight.setText(String.valueOf(theLocation.getHeight()));
+                                textLocationWidth.setText(String.valueOf(theLocation.getWidth()));
+                            } else{
+                                textLocationName.setText("");
+                                textLocationHeight.setText("");
+                                textLocationWidth.setText("");
+                            }
+                        });
+                        collumnUpdate.getItems().add(newButton);
+
+                        // Update combobox
+                        locationComboBox.getItems().add(location);
+
+                        // Update textboxes
+                        textLocationName.setText("");
+                        textLocationHeight.setText("");
+                        textLocationWidth.setText("");
+
+                        refreshLocations();
+                        add = true;
+                    } catch (Exception e){
+                        locationAdded.setText("Location could not be updated!");
+                        e.printStackTrace();
                     }
                 }
             } else {
-                locationAdded.setText("Location was not added!");
+                locationAdded.setText("Please enter valid texts!");
             }
         });
         locationAddBox.getChildren().addAll(boxLocationName, boxLocationHeight, boxLocationWidth, buttonLocationAdd, locationAdded);
 
         // Delete location
         VBox locationUpdateDeleteBox = new VBox();
-        HashMap<Integer, Location> locationMap = schedule.getLocations();
         for (int i = 1; i <= locations.size(); i++) {
             locationComboBox.getItems().add(locations.get(i));
         }
@@ -180,25 +253,35 @@ public class LocationsOverview{
         Label locationDeleted = new Label();
         buttonLocationDelete.setOnAction(event -> {
             if (locationComboBox.getValue() != null){
-                int index = 0;
-                for (Map.Entry<Integer, Location> entry : locationMap.entrySet()){
-                    if (entry.getValue() == locationComboBox.getValue()){
-                        // Delete location from lists and schedule
-                        schedule.deleteLocation(entry.getValue().getId());
-                        locations.remove(entry.getKey(), entry.getValue());
-
-                        // Update list
-                        collumnHeights.getItems().remove(index+1);
-                        collumnWidths.getItems().remove(index+1);
-                        collumnNames.getItems().remove(index+1);
-                        collumnVisitors.getItems().remove(index+1);
-
-                        // Update combobox
-                        locationComboBox.getItems().remove(entry.getValue());
+                try {
+                    // Get id
+                    int index = 0;
+                    int theIndex = 0;
+                    for (Map.Entry<Integer, Location> entry : locations.entrySet()){
+                        index++;
+                        if (entry.getValue() == locationComboBox.getValue()){
+                            theIndex = index;
+                        }
                     }
-                    index++;
+                    // Delete location from lists and schedule
+                    schedule.deleteLocation(locationComboBox.getValue().getId());
+                    locations.remove(locationComboBox.getValue().getId(), locationComboBox.getValue());
+
+                    // Update lists
+                    collumnHeights.getItems().remove(theIndex);
+                    collumnWidths.getItems().remove(theIndex);
+                    collumnNames.getItems().remove(theIndex);
+                    collumnVisitors.getItems().remove(theIndex);
+                    collumnUpdate.getItems().remove(theIndex);
+
+                    // Update combobox
+                    locationComboBox.getItems().remove(locationComboBox.getValue());
+                    locationDeleted.setText("Location deleted!");
+
+                    refreshLocations();
+                } catch (Exception e){
+                    e.printStackTrace();
                 }
-                locationDeleted.setText("Location deleted!");
             } else {
                 locationDeleted.setText("Location was not deleted!");
             }
@@ -206,8 +289,17 @@ public class LocationsOverview{
         locationUpdateDeleteBox.getChildren().addAll(locationComboBox, buttonLocationDelete, locationDeleted);
 
         mainBox.getChildren().addAll(listsContainerBox, locationAddBox, locationUpdateDeleteBox);
-        //schedule.getLocations();
-
         return mainBox;
+    }
+
+    public static void refreshLocations(){
+        // Update locations list & (re)check id values
+        locations = schedule.getLocations();
+        hiddenId = locations.size()+1;
+        for (Map.Entry<Integer, Location> entry : locations.entrySet()){
+            if (entry.getKey() == hiddenId){
+                hiddenId++;
+            }
+        }
     }
 }
