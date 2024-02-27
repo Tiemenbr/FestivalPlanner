4c9f07863a7a1c1a6a63da4f20422f5a644bf5e9
tiemenbr <tiemenbr@hotmail.nl>
2024-02-27 12:16:43 +0100

    renamed variables

--- src/gui/ScheduleView.java
+++ src/gui/ScheduleView.java
@@ -12,24 +12,24 @@ public class ScheduleView {
     public static Node createScheduleView(Schedule schedule) {
         GridPane layout = new GridPane();
         for (int i = 0; i < 24; i++) {
-            Label label = new Label(String.format("%s:00", i));
+            Label hour = new Label(String.format("%s:00", i));
             if (i%2==0) {
-                Pane pane = new Pane();
-                pane.setBackground(new Background(
+                Pane grayPane = new Pane();
+                grayPane.setBackground(new Background(
                         new BackgroundFill(Color.valueOf("#808080"),
                         new CornerRadii(0),
                         new Insets(0))));
                 // value 10 rowspan should be replaced with amount of locations
-                layout.add(pane,i+1,0, 1,10);
-                GridPane.setVgrow(pane, Priority.ALWAYS);
+                layout.add(grayPane,i+1,0, 1,10);
+                GridPane.setVgrow(grayPane, Priority.ALWAYS);
             }
-            layout.add(label,i+1,0);
-            GridPane.setHgrow(label,Priority.ALWAYS);
+            layout.add(hour,i+1,0);
+            GridPane.setHgrow(hour,Priority.ALWAYS);
         }
-        Label test = new Label("Location 1");
-        layout.add(test,0,1);
-        GridPane.setVgrow(test, Priority.ALWAYS);
-        GridPane.setValignment(test, VPos.TOP);
+        Label testLocation = new Label("Location 1");
+        layout.add(testLocation,0,1);
+        GridPane.setVgrow(testLocation, Priority.ALWAYS);
+        GridPane.setValignment(testLocation, VPos.TOP);
         return layout;
     }
 }
