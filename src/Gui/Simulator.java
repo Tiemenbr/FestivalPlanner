8fc30cdb3b8d83a04cca2888622e9f22a21ac24c
DaanF1 <daanstander@hotmail.nl>
2024-04-01 13:48:26 +0200

    Fixed locations loading bug
    Locations are now loaded in at the start of the application

--- src/Gui/Simulator.java
+++ src/Gui/Simulator.java
@@ -3,6 +3,7 @@ package Gui;
 import Gui.SimulatorView.MapGenerator;
 import Gui.SimulatorView.SpriteSheetHelper;
 import Gui.SimulatorView.Visitor;
+import Objects.Attraction;
 import Objects.Location;
 import Objects.Schedule;
 import javafx.animation.AnimationTimer;
@@ -17,14 +18,15 @@ import java.util.ArrayList;
 public class Simulator{
     // TODO: fix zooming bug (not centered in the middle of the screen)
     private static Schedule schedule = Planner.getSCHEDULE();
+    private static final MapGenerator mapGenerator = Schedule.getMapGenerator();
     private static Canvas canvas;
     private static VBox vBox;
     private static Camera camera;
-    private static final MapGenerator mapGenerator = new MapGenerator("festivalMap.json");
     static ArrayList<Visitor> visitors = new ArrayList<>();
     private static SpriteSheetHelper spriteSheetHelper;
     private static int visitorAmount = 100;
     private static ArrayList<Location> locations = new ArrayList<>();
+    private static ArrayList<Attraction> attractions = new ArrayList<>();
 
     public static VBox getComponent() {
         vBox = new VBox();
@@ -68,15 +70,19 @@ public class Simulator{
     }
 
     public static void init() {
-        // Get all locations
-        for (Location location : mapGenerator.getLocations()){
-            schedule.addLocation(location);
-        }
+        // Get Locations & Attractions
         locations.addAll(schedule.getLocations().values());
+        attractions.addAll(schedule.getAttractions().values());
+
+        // Debug code:
+        for (Attraction a : attractions){
+            System.out.println(a);
+        }
 
         spriteSheetHelper = new SpriteSheetHelper();
 //        BufferedImage[] vistorSprites1 = spriteSheetHelper.createSpriteSheet("/walk template 2.png", 4);
 
+        // Starting visitor amount
         while(visitors.size() < 10) {
             addVisitor();
         }
